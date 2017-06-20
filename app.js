03feb384e9d6e39c1d82ed3d0b95ea6e689ac25e
Karan Singh <kar.n@icloud.com>
2017-06-19 17:59:14 -0700

    blogs can now be found using ids

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 const path = require('path');
 // local dependencies.
-  const mongoose = require('./db/mongoose');
-  const Blog = require('./db/models/blogs');
+const mongoose = require('./db/mongoose');
+const Blog = require('./db/models/blogs');
 
 // immutabile variables
 const app = express();
@@ -15,13 +15,13 @@ const port = process.env.port || 3000;
 // this will parse the body of the incoming request
 // as well as set absolute path for public
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join( __dirname, 'public' )));
 
 // connecting mongoose
 
 // routes
 // CREATE
-app.post('/blogs', (req, res)=>{
+app.post('/blogs', ( req, res )=>{
   // capturing the inputs
 	var blog = new Blog({
 		title: req.body.title,
@@ -36,7 +36,22 @@ app.post('/blogs', (req, res)=>{
 });
 
 //READ
+app.get('/blogs/', ( req, res )=>{
+	Blog.find({}).then((blogs)=>{
+		res.send(blogs);
+	}).catch( e => res.status(400).send(e) );
+});
+
+// READBYID
+app.get('/blogs/:id', ( req, res )=>{
+	let id = req.params.id;
+	Blog.findById({_id: id}).then((blogs)=>{
+		res.send(blogs);
+	}).catch( e => res.status(400).send(e) );
+});
+
 //UPDATE
+
 //DELETE
 
 app.listen(port, ()=> console.log(`Server is up on ${port}`));
